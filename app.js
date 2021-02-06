7e2b613000e73d84408f9b6205fa4139a428e704
HyeongGu0401 <boxing0401@naver.com>
2021-02-06 20:37:41 +0900

    Test

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ var app = express()
 app.listen(3000, function () {
     console.log("server on");
 })
+
+// static 디렉토리
+app.use(express.static('public'))
+
+// url routing
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/main.html')
+})
