a442150aba37aaaf3ec3beab9ff0390c328f1c5f
HyeongGu0401 <boxing0401@naver.com>
2021-02-08 14:21:05 +0900

    Test

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
 app.listen(3000, function () {
     console.log("server on");
@@ -7,8 +8,19 @@ app.listen(3000, function () {
 
 // static 디렉토리
 app.use(express.static('public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.set('view engine', 'ejs');
 
 // url routing
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/main.html')
+});
+
+// POST 요청
+app.post('/email_post', function (req, res) {
+    console.log(req.body.email)
+    res.render('email.ejs', {
+        'email': req.body.email
+    })
 })
